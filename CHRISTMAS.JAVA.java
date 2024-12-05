9eda572139dde18ed8c88d6f783ae1fb0162e8e8
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-05 15:53:20 +0530

    Add files via upload

---
+++ CHRISTMAS.JAVA.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        
+        if (X == 25) {
+            System.out.println("CHRISTMAS");
+        } else {
+            System.out.println("ORDINARY");
+        }
+        
+        scanner.close();
+    }
+}
